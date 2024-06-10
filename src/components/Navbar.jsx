24cdfe22d20281b253ac08c7423cf40b8a89d196
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-06-10 09:22:50 +0700

    tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white h-20 shadow-md top-0 fixed w-full flex justify-between px-20'>
+        <div className="font-extrabold text-xl my-auto">Farid_Dev</div>
+        <div className="flex my-auto space-x-10 font-semibold">
+            <a href="">Home</a>
+            <a href="">Home</a>
+            <a href="">Home</a>
+            <a href="">Home</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
