25138f6978bc034a49602d9b19bfdc2bf3e58ee5
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-06-10 23:29:07 +0700

    done bang

--- src/page/about/index.jsx
+++ src/page/about/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import BlobAnimation from "../../components/BlobAnimation";
 import { TypeAnimation } from "react-type-animation";
+import { Link } from "react-router-dom";
+
 
 const index = () => {
   const TransitionVariants = {
@@ -67,7 +69,24 @@ const index = () => {
               repeat={Infinity}
             />
           </h1>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="https://drive.google.com/file/d/1XF_LRNgc6VSiI0DVfLp6F7HROl3zvTkX/view?usp=sharing"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
         </h1>
+        
         <div className="w-full xl:w-1/2 justify-center mx-auto flex">
         <div className=" rounded-full bg-gradient-to-br from-purple-600 to bg-red-500 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
           <img
@@ -77,9 +96,11 @@ const index = () => {
             width={300}
             height={300}
           />
+          
         </div>
         </div>
       </div>
+      
     </div>
   );
 };
