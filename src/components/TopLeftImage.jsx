d920f10f487da47d0c62dba0dcf93253b3b3184e
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-06-10 12:48:20 +0700

    img

--- src/components/TopLeftImage.jsx
+++ src/components/TopLeftImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TopLeftImage = () => {
   return (
     <div className='absolute top-0 left-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50'>
-        <img width={400} height={400} src="./public/top-left-img.png" alt="" />
+        <img width={400} height={400} src="./top-left-img.png" alt="" />
     </div>
   )
 }
