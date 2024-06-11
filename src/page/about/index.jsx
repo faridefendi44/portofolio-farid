acf14a065698467bd9b900b2b4fd239053aab34d
faridefendi44 <faridnurefendi@student.ub.ac.id>
2024-06-11 08:07:02 +0700

    cv

--- src/page/about/index.jsx
+++ src/page/about/index.jsx
@@ -69,7 +69,7 @@ const index = () => {
               repeat={Infinity}
             />
           </h1>
-          <div>
+          {/* <div>
             <Link
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
@@ -84,7 +84,7 @@ const index = () => {
                 Download CV
               </span>
             </a>
-          </div>
+          </div> */}
         </h1>
         
         <div className="w-full xl:w-1/2 justify-center mx-auto flex">
